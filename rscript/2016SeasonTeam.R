c93e31a94f79098268f777f61b159010bcce07b3
Marco Chiapello <mc983.cam@gmail.com>
2017-03-17 22:57:42 +0000

    New plot with polar coordinates

--- rscript/2016SeasonTeam.R
+++ rscript/2016SeasonTeam.R
@@ -10,7 +10,7 @@ x %>% group_by(team) %>% mutate(cum = cumsum(MURO_Prf.)) %>% ggplot(aes(x = Gior
 	geom_line() + geom_point() +
 	theme_bw() +
 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7)) +
-	ylab("MURO Perfetto")
+	ylab("MURO Perfetto") + coord_polar()
 
 # Plot PUNTI
 x %>% group_by(team) %>% mutate(cum = cumsum(PUNTI_Tot)) %>% ggplot(aes(x = Giornata, y = cum,
@@ -19,13 +19,22 @@ x %>% group_by(team) %>% mutate(cum = cumsum(PUNTI_Tot)) %>% ggplot(aes(x = Gior
 	theme_bw() +
 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7)) +
 	ylab("Punti Totali")
-x$ATTACCO_Err. <- as.numeric(x$ATTACCO_Err.)
+
 # Plot Attacco
+x$ATTACCO_Err. <- as.numeric(x$ATTACCO_Err.)
 x %>% group_by(team) %>% mutate(ma = mean(ATTACCO_Err.), cumE = cumsum(as.numeric(ATTACCO_Err.))) %>% ggplot(aes(x = Giornata, y = ATTACCO_Err.,
 					 group = team, colour = team)) +
 	geom_line() + geom_point() + 
 	theme_bw() +
 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7)) +
-	ylab("Attacchi Errati") + 	facet_wrap(~team)
+	ylab("Attacchi Errati") + facet_wrap(~team)
+
+y <- x %>% group_by(team) %>% summarize(m = mean(ATTACCO_Err.))
+x %>% group_by(team) %>% mutate(ma = mean(ATTACCO_Err.), cumE = cumsum(as.numeric(ATTACCO_Err.))) %>% ggplot(aes(x = Giornata, y = ATTACCO_Err.,
+					 group = team, colour = team)) +
+	geom_line() + geom_point() + geom_line(aes(x = Giornata, y = ma), colour = "black", size = .3) +
+	theme_bw() +
+	theme(axis.text.y = element_blank(), axis.text.x = element_blank(), legend.position = "none", axis.ticks.y = element_blank(), panel.grid.major = element_blank(), plot.title = element_text(hjust=0.5, size = 15)) + geom_text(data = y, aes(x = 2, y = 17, label = paste0("Errori medi: ", round(m,2))), size = 2, colour = "black") +
+	ylab("") + xlab("") + labs(title = "ATTACCHI ERRATI") + coord_polar() + facet_wrap(~team)
 
 
