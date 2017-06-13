26d331d2562b85f27adcbf3af9ccea56224ca7b2
Marco Chiapello <mc983.cam@gmail.com>
2017-06-13 22:22:08 +0100

    Scraping all teams

--- rscript/player.R
+++ rscript/player.R
@@ -30,3 +30,18 @@ oo <- oo %>% filter(Atleta %in% c("Sabbi Giulio", "Hernandez Ramos Fernando", "M
 oo$giornata <- factor(oo$giornata, levels = unique(oo$giornata))
 ggplot(data = oo, aes(x = giornata, y = values, group = Atleta, colour = Atleta)) + geom_point(alpha = .5) + geom_line(alpha = .3) + theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7))
 
+# Load data
+load("../data/stat_per_player_ricez.rda", verbose = TRUE)
+
+####### Heatmap results
+# Create df
+final2 <- xx %>% gather(giornata, values, -Atleta, -Squadra)
+oo <- final2 %>% filter(grepl("^Ricez_", giornata)) %>% print(n = 100)
+oo$giornata <- factor(oo$giornata, levels = unique(oo$giornata))
+ggplot(data = oo, aes(x = giornata, y = values, group = Atleta, colour = Atleta)) + geom_point() + geom_line() + theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7)) 
+
+oo <- final2 %>% filter(grepl("^DiffRicez_", giornata)) %>% print(n = 100)
+oo <- oo %>% filter(Atleta %in% c("Clevenot Trevor", "Botto Iacopo", "Maruotti Gabriele"))
+oo$giornata <- factor(oo$giornata, levels = unique(oo$giornata))
+ggplot(data = oo, aes(x = giornata, y = values, group = Atleta, colour = Atleta)) + geom_point(alpha = .5) + geom_line(alpha = .3) + theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5, size = 7)) + geom_smooth(method = "loess", se = FALSE)
+
--- rscript/scraping.R
+++ rscript/scraping.R
@@ -6,7 +6,16 @@ library(tidyverse)
 ######################### season 2016 ############################
 ## Download data
 # Create a list of the teams
-list <- data.frame(name = c("Top Volley Latina", "Cucine Lube Civitanova", "Revivre Milano", "Gi Group Monza", "Azimut Modena", "Exprivia Molfetta", "Kioene Padova", "Sir Safety Conad Perugia", "LPR Piacenza", "Bunge Ravenna", "Biosi Indexa Sora", "Diatec Trentino", "Calzedonia Verona", "Tonno Callipo Calabria Vibo Valentia"), code = c("LT", "MC", "MI-POWER", "MIVER", "MO", "BAM", "PD", "BASTIA", "PC", "RAV-ROB", "FR-SORA", "TN-ITAS", "VRI", "VV"))
+list <- data.frame(name = c("Top Volley Latina", "Cucine Lube Civitanova",
+							"Revivre Milano", "Gi Group Monza", "Azimut Modena",
+							"Exprivia Molfetta", "Kioene Padova",
+							"Sir Safety Conad Perugia", "LPR Piacenza",
+							"Bunge Ravenna", "Biosi Indexa Sora",
+							"Diatec Trentino", "Calzedonia Verona",
+							"Tonno Callipo Calabria Vibo Valentia"),
+				   code = c("LT", "MC", "MI-POWER", "MIVER", "MO", "BAM", "PD",
+							"BASTIA", "PC", "RAV-ROB", "FR-SORA", "TN-ITAS",
+							"VRI", "VV"))
 
 # Download Squadre giornata per giornata
 y <- list()
@@ -135,81 +144,44 @@ def <- tbl_df(transform(def, SetGioc. = as.numeric(SetGioc.),
 save(def, file = "../data/season2016.rda")
 
 ###############################################################################
-## STATISTICS per team
+## STATISTICS per team TOTALI
 ###############################################################################
-######### Girone di andata
 a <- list()
 
 ## URL giornata
-sq <- 5837:5849
-
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
 ## Download tabelle girone di andata
-for (i in 1:13){
+for (i in 1:26){
 	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.1&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
 	# Extract the data table
-	a[[i]] <- y[[5]]
-	# Remove unused columns
-	a[[i]] <- a[[i]][, c(2, 4, 5)]
+	a[[i]] <- y[[5]][, c(2, 4:5)]
 	# Rename tables
-	names(a[[i]])[2:3] <- paste0(names(a[[i]])[2:3], "_andata_", i)
+	names(a[[i]])[2:3] <- paste0(c("Set", "Totali"), "_", gir[i], "_", rep(1:13, 2)[i])
 }
 
 ## Combine list into df
-andata <- a[[1]]
-for (i in 2:13){
-	andata <- left_join(andata, a[[i]], by = c("Club"))
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = "Club")
 }
 
-# Reorder
-andata <- andata[, c(1, seq(2, 26, 2), seq(3, 27, 2))]
-
-######## Girone di ritorno
-a <- list()
-
-## URL giornata
-sq <- 5850:5862
-
-## Download tabelle girone di ritorno
-for (i in 1:13){
-	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.1&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
-	# Extract the data table
-	a[[i]] <- y[[5]]
-	# Remove unused columns
-	a[[i]] <- a[[i]][, c(2, 4, 5)]
-	# Rename tables
-	names(a[[i]])[2:3] <- paste0(names(a[[i]])[2:3], "_ritorno_", i)
-	# Remove dot
-}
-
-## Combine list into df
-ritorno <- a[[1]]
-for (i in 2:13){
-	ritorno <- left_join(ritorno, a[[i]], by = c("Club"))
-}
-
-# Reorder
-ritorno <- ritorno[, c(1, seq(2, 26, 2), seq(3, 27, 2))]
+## Reorder
+x <- x[, c(1, seq(2, 52, 2), seq(3, 53, 2))]
 
 # Remove decimals
-for (i in 1:nrow(ritorno)){
-	for (ii in 15:ncol(ritorno)){
-		if (ritorno[i, ii] < 2){
-			ritorno[i, ii] <- ritorno[i, ii] * 1000
+for (i in 1:nrow(x)){
+	for (ii in 1:ncol(x)){
+		if (x[i, ii] < 2){
+			x[i, ii] <- x[i, ii] * 1000
 		} else {
-			ritorno[i, ii] <- ritorno[i, ii]
+			x[i, ii] <- x[i, ii]
 		}
 	}
 }
 
-
-###############################################################################
-# Combine Andata + Ritorno
-x <- inner_join(andata, ritorno, by = "Club")
-# Reorder
-x <- x[, c(1, 2:14, 28:40, 15:27, 41:53)]
-
 # Cumulative difference
-m <- matrix(rep(0, nrow(x) * (ncol(x)-1)), nrow = 14)
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 1)), nrow = 14)
 for (i in 1:nrow(x)){
 	m[i, 1:26] <- c(as.numeric(x[i, 2:27])[1], diff(as.numeric(x[i, 2:27])))
 	m[i, 27:52] <- c(as.numeric(x[i, 28:53])[1], diff(as.numeric(x[i, 28:53])))
@@ -217,77 +189,229 @@ for (i in 1:nrow(x)){
 
 # Rename columns
 colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
-				 paste0("DiffPunti_andata_", 1:13), paste0("DiffPunti_ritorno_", 1:13))
+				 paste0("DiffTot_andata_", 1:13), paste0("DiffTot_ritorno_", 1:13))
 
 # Combine datasets
-x <- tbl_df(cbind(x, m))
+xx <- tbl_df(cbind(x, m))
 
 # Save stat per team
-save(x, file = "../data/stat_per_team.rda")
+save(xx, file = "../data/stat_per_team_tot.rda")
+
+###############################################################################
+## STATISTICS per team ATTACCO
+###############################################################################
+a <- list()
+
+## URL giornata
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
+## Download tabelle girone di andata
+for (i in 1:26){
+	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.3&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
+	# Extract the data table
+	a[[i]] <- y[[5]][, c(2, 4:5)]
+	# Rename tables
+	names(a[[i]])[2:3] <- paste0(c("Set", "Att"), "_", gir[i], "_", rep(1:13, 2)[i])
+}
+
+## Combine list into df
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = "Club")
+}
+
+## Reorder
+x <- x[, c(1, seq(2, 52, 2), seq(3, 53, 2))]
+
+# Remove decimals
+for (i in 1:nrow(x)){
+	for (ii in 1:ncol(x)){
+		if (x[i, ii] < 2){
+			x[i, ii] <- x[i, ii] * 1000
+		} else {
+			x[i, ii] <- x[i, ii]
+		}
+	}
+}
+
+# Cumulative difference
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 1)), nrow = 14)
+for (i in 1:nrow(x)){
+	m[i, 1:26] <- c(as.numeric(x[i, 2:27])[1], diff(as.numeric(x[i, 2:27])))
+	m[i, 27:52] <- c(as.numeric(x[i, 28:53])[1], diff(as.numeric(x[i, 28:53])))
+}
+
+# Rename columns
+colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
+				 paste0("DiffAtt_andata_", 1:13), paste0("DiffAtt_ritorno_", 1:13))
+
+# Combine datasets
+xx <- tbl_df(cbind(x, m))
+
+# Save stat per team
+save(xx, file = "../data/stat_per_team_att.rda")
+
+###############################################################################
+## STATISTICS per team BATTUTA
+###############################################################################
+a <- list()
+
+## URL giornata
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
+## Download tabelle girone di andata
+for (i in 1:26){
+	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.5&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
+	# Extract the data table
+	a[[i]] <- y[[5]][, c(2, 4:5)]
+	# Rename tables
+	names(a[[i]])[2:3] <- paste0(c("Set", "ACE"), "_", gir[i], "_", rep(1:13, 2)[i])
+}
+
+## Combine list into df
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = "Club")
+}
+
+## Reorder
+x <- x[, c(1, seq(2, 52, 2), seq(3, 53, 2))]
+
+# Cumulative difference
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 1)), nrow = 14)
+for (i in 1:nrow(x)){
+	m[i, 1:26] <- c(as.numeric(x[i, 2:27])[1], diff(as.numeric(x[i, 2:27])))
+	m[i, 27:52] <- c(as.numeric(x[i, 28:53])[1], diff(as.numeric(x[i, 28:53])))
+}
+
+# Rename columns
+colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
+				 paste0("DiffAtt_andata_", 1:13), paste0("DiffAtt_ritorno_", 1:13))
+
+# Combine datasets
+xx <- tbl_df(cbind(x, m))
+
+# Save stat per team
+save(xx, file = "../data/stat_per_team_ace.rda")
+
+###############################################################################
+## STATISTICS per team RICEZIONE
+###############################################################################
+a <- list()
+
+## URL giornata
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
+## Download tabelle girone di andata
+for (i in 1:26){
+	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.7&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
+	# Extract the data table
+	a[[i]] <- y[[5]][, c(2, 4:5)]
+	# Rename tables
+	names(a[[i]])[2:3] <- paste0(c("Set", "Ric"), "_", gir[i], "_", rep(1:13, 2)[i])
+}
+
+## Combine list into df
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = "Club")
+}
+
+## Reorder
+x <- x[, c(1, seq(2, 52, 2), seq(3, 53, 2))]
+
+# Cumulative difference
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 1)), nrow = 14)
+for (i in 1:nrow(x)){
+	m[i, 1:26] <- c(as.numeric(x[i, 2:27])[1], diff(as.numeric(x[i, 2:27])))
+	m[i, 27:52] <- c(as.numeric(x[i, 28:53])[1], diff(as.numeric(x[i, 28:53])))
+}
+
+# Rename columns
+colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
+				 paste0("DiffAtt_andata_", 1:13), paste0("DiffAtt_ritorno_", 1:13))
+
+# Combine datasets
+xx <- tbl_df(cbind(x, m))
+
+# Save stat per team
+save(xx, file = "../data/stat_per_team_ricez.rda")
+
+###############################################################################
+## STATISTICS per team MURO
+###############################################################################
+a <- list()
+
+## URL giornata
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
+## Download tabelle girone di andata
+for (i in 1:26){
+	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=1&Classifica=1.9&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i]), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
+	# Extract the data table
+	a[[i]] <- y[[5]][, c(2, 4:5)]
+	# Rename tables
+	names(a[[i]])[2:3] <- paste0(c("Set", "Muri"), "_", gir[i], "_", rep(1:13, 2)[i])
+}
+
+## Combine list into df
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = "Club")
+}
+
+## Reorder
+x <- x[, c(1, seq(2, 52, 2), seq(3, 53, 2))]
+
+# Cumulative difference
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 1)), nrow = 14)
+for (i in 1:nrow(x)){
+	m[i, 1:26] <- c(as.numeric(x[i, 2:27])[1], diff(as.numeric(x[i, 2:27])))
+	m[i, 27:52] <- c(as.numeric(x[i, 28:53])[1], diff(as.numeric(x[i, 28:53])))
+}
+
+# Rename columns
+colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
+				 paste0("DiffMuri_andata_", 1:13), paste0("DiffMuri_ritorno_", 1:13))
+
+# Combine datasets
+xx <- tbl_df(cbind(x, m))
+
+# Save stat per team
+save(xx, file = "../data/stat_per_team_muro.rda")
 
 ###############################################################################
 ## Stat per atleta TOTALI
 ###############################################################################
-######### Girone di andata
 a <- list()
 
 ## URL giornata
-sq <- 5837:5849
-
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
 ## Download tabelle girone di andata
-for (i in 1:13){
+for (i in 1:26){
 	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=2&Classifica=2.1&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i], "&Pos=40"), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
 	# Extract the data table
 	a[[i]] <- y[[5]][, c(2, 4, 6:7)]
 	# Rename tables
-	names(a[[i]])[3:4] <- paste0(c("Set", "Punti"), "_andata_", i)
+	names(a[[i]])[3:4] <- paste0(c("Set", "Totali"), "_", gir[i], "_", rep(1:13, 2)[i])
 }
 
 ## Combine list into df
-andata <- a[[1]]
-for (i in 2:13){
-	andata <- left_join(andata, a[[i]], by = c("Atleta", "Squadra"))
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = c("Atleta", "Squadra"))
 }
 
 ## Reorder
-andata <- andata[, c(1, 2, seq(3, 27, 2), seq(4, 28, 2))]
-
-######### Girone di ritorno
-a <- list()
-
-## URL giornata
-sq <- 5850:5862
-
-## Download tabelle girone di ritorno
-for (i in 1:13){
-	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=2&Classifica=2.1&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i], "&Pos=40"), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
-	# Extract the data table
-	a[[i]] <- y[[5]][, c(2, 4, 6:7)]
-	# Rename tables
-	names(a[[i]])[3:4] <- paste0(c("Set", "Punti"), "_ritorno_", i)
-}
-
-## Combine list into df
-ritorno <- a[[1]]
-for (i in 2:13){
-	ritorno <- left_join(ritorno, a[[i]], by = c("Atleta", "Squadra"))
-}
-
-## Reorder
-ritorno <- ritorno[, c(1, 2, seq(3, 27, 2), seq(4, 28, 2))]
-
-###############################################################################
-# Combine andata + ritorno
-x <- inner_join(andata, ritorno, by = c("Atleta", "Squadra"))
-
-# Reorder
-x <- x[, c(1, 2, 3:15, 29:41, 16:28, 42:54)]
+x <- x[, c(1, 2, seq(3, 53, 2), seq(4, 54, 2))]
 
 # Sort
 x <- x[order(x[, 54], decreasing = TRUE), ]
+x <- x[1:10,]
 
 # Cumulative difference
-m <- matrix(rep(0, nrow(x) * (ncol(x) - 2)), nrow = 27)
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 2)), nrow = 10)
 for (i in 1:nrow(x)){
 	m[i, 1:26] <- c(as.numeric(x[i, 3:28])[1], diff(as.numeric(x[i, 3:28])))
 	m[i, 27:52] <- c(as.numeric(x[i, 29:54])[1], diff(as.numeric(x[i, 29:54])))
@@ -295,11 +419,10 @@ for (i in 1:nrow(x)){
 
 # Rename columns
 colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
-				 paste0("DiffPunti_andata_", 1:13), paste0("DiffPunti_ritorno_", 1:13))
+				 paste0("DiffTot_andata_", 1:13), paste0("DiffTot_ritorno_", 1:13))
 
 # Combine datasets
 xx <- tbl_df(cbind(x, m))
-xx <- xx[1:10, ]
 
 # Save stat per team
 save(xx, file = "../data/stat_per_player_tot.rda")
@@ -307,66 +430,35 @@ save(xx, file = "../data/stat_per_player_tot.rda")
 ###############################################################################
 ## Stat per atleta ATTACCO
 ###############################################################################
-######### Girone di andata
 a <- list()
 
 ## URL giornata
-sq <- 5837:5849
-
+sq <- 5837:5862
+gir <- rep(c("andata", "ritorno"), each = 13)
 ## Download tabelle girone di andata
-for (i in 1:13){
+for (i in 1:26){
 	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=2&Classifica=2.3&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i], "&Pos=40"), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
 	# Extract the data table
 	a[[i]] <- y[[5]][, c(2, 4, 6:7)]
 	# Rename tables
-	names(a[[i]])[3:4] <- paste0(c("Set", "Punti"), "_andata_", i)
+	names(a[[i]])[3:4] <- paste0(c("Set", "Attacco"), "_", gir[i], "_", rep(1:13, 2)[i])
 }
 
 ## Combine list into df
-andata <- a[[1]]
-for (i in 2:13){
-	andata <- left_join(andata, a[[i]], by = c("Atleta", "Squadra"))
+x <- a[[1]]
+for (i in 2:26){
+	x <- left_join(x, a[[i]], by = c("Atleta", "Squadra"))
 }
 
 ## Reorder
-andata <- andata[, c(1, 2, seq(3, 27, 2), seq(4, 28, 2))]
-
-######### Girone di ritorno
-a <- list()
-
-## URL giornata
-sq <- 5850:5862
-
-## Download tabelle girone di ritorno
-for (i in 1:13){
-	y <- read_html(paste0("http://www.legavolley.it/Rendimento.asp?Tipo=2&Classifica=2.3&AnnoInizio=2016&AnnoFine=2016&Serie=1&Fase=1&Giornata=", sq[i], "&Pos=40"), encoding = "ISO-8859-1") %>% html_table(fill = TRUE)
-	# Extract the data table
-	a[[i]] <- y[[5]][, c(2, 4, 6:7)]
-	# Rename tables
-	names(a[[i]])[3:4] <- paste0(c("Set", "Punti"), "_ritorno_", i)
-}
-
-## Combine list into df
-ritorno <- a[[1]]
-for (i in 2:13){
-	ritorno <- left_join(ritorno, a[[i]], by = c("Atleta", "Squadra"))
-}
-
-## Reorder
-ritorno <- ritorno[, c(1, 2, seq(3, 27, 2), seq(4, 28, 2))]
-
-###############################################################################
-# Combine andata + ritorno
-x <- inner_join(andata, ritorno, by = c("Atleta", "Squadra"))
-
-# Reorder
-x <- x[, c(1, 2, 3:15, 29:41, 16:28, 42:54)]
+x <- x[, c(1, 2, seq(3, 53, 2), seq(4, 54, 2))]
 
 # Sort
 x <- x[order(x[, 54], decreasing = TRUE), ]
+x <- x[1:10,]
 
 # Cumulative difference
-m <- matrix(rep(0, nrow(x) * (ncol(x) - 2)), nrow = 28)
+m <- matrix(rep(0, nrow(x) * (ncol(x) - 2)), nrow = 10)
 for (i in 1:nrow(x)){
 	m[i, 1:26] <- c(as.numeric(x[i, 3:28])[1], diff(as.numeric(x[i, 3:28])))
 	m[i, 27:52] <- c(as.numeric(x[i, 29:54])[1], diff(as.numeric(x[i, 29:54])))
@@ -374,11 +466,10 @@ for (i in 1:nrow(x)){
 
 # Rename columns
 colnames(m) <- c(paste0("DiffSet_andata_", 1:13), paste0("DiffSet_ritorno_", 1:13),
-				 paste0("DiffPunti_andata_", 1:13), paste0("DiffPunti_ritorno_", 1:13))
+				 paste0("DiffAtt_andata_", 1:13), paste0("DiffAtt_ritorno_", 1:13))
 
 # Combine datasets
 xx <- tbl_df(cbind(x, m))
-xx <- xx[1:10, ]
 
 # Save stat per team
 save(xx, file = "../data/stat_per_player_att.rda")
@@ -478,7 +569,7 @@ xx <- tbl_df(cbind(x, m))
 save(xx, file = "../data/stat_per_player_ricez.rda")
 
 ###############################################################################
-## Stat per atleta RICEZIONE
+## Stat per atleta MURO
 ###############################################################################
 a <- list()
 
